868e5aebdae475dae52268d67bc20f351554e3a6
UnknownMysteryOfficial <116437970+UnknownMysteryOfficial@users.noreply.github.com>
2024-12-31 14:25:38 +0500

    Add files via upload

--- src/main/java/org/mortisdevelopment/mortisallinone/MortisAllinOne.java
+++ src/main/java/org/mortisdevelopment/mortisallinone/MortisAllinOne.java
@@ -3,8 +3,13 @@ package org.mortisdevelopment.mortisallinone;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.PluginCommand;
+import org.bukkit.command.SimpleCommandMap;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.mortisdevelopment.mortisallinone.generator.*;
 import org.mortisdevelopment.mortisallinone.homes.HomeTeleportCommand;
@@ -20,7 +25,11 @@ import org.mortisdevelopment.mortisallinone.warp.SetWarpCommand;
 import org.mortisdevelopment.mortisallinone.warp.WarpTeleportCommand;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
+
+import static org.bukkit.Bukkit.getLogger;
 
 public final class MortisAllinOne extends JavaPlugin {
 
@@ -32,9 +41,17 @@ public final class MortisAllinOne extends JavaPlugin {
         this.worldManager = new WorldManager(this);
         loadWorlds();
         saveDefaultConfig();
+        File file = getFile("homes.yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(file);
+        File file2 = getFile("warps.yml");
+        FileConfiguration config2 = YamlConfiguration.loadConfiguration(file2);
+
+        if (!getConfig().getBoolean("rtp-enable", true)) {
+            unregisterCommand("rtp");
+        }
 
         getLogger().info("Mortis All-in-One was successfully loaded!");
-        getLogger().info("Running version 1.5");
+        getLogger().info("Running version 1.6");
         getCommand("setspawn").setExecutor(new SpawnPointCommand(this));
         getCommand("spawn").setExecutor(new SpawnTeleportCommand(this));
         getServer().getPluginManager().registerEvents(new SpawnTeleportListener(this), this);
@@ -78,6 +95,30 @@ public final class MortisAllinOne extends JavaPlugin {
         return worldManager;
     }
 
+
+    private void unregisterCommand(String commandName) {
+        try {
+            PluginManager pluginManager = Bukkit.getPluginManager();
+
+            Field commandMapField = pluginManager.getClass().getDeclaredField("commandMap");
+            commandMapField.setAccessible(true);
+            CommandMap commandMap = (CommandMap) commandMapField.get(pluginManager);
+
+            Field knownCommandsField = SimpleCommandMap.class.getDeclaredField("knownCommands");
+            knownCommandsField.setAccessible(true);
+            @SuppressWarnings("unchecked")
+            Map<String, Command> knownCommands = (Map<String, Command>) knownCommandsField.get(commandMap);
+
+            if (knownCommands.containsKey(commandName)) {
+                knownCommands.remove(commandName);
+                getLogger().info("Successfully unregistered the /" + commandName + " command.");
+            } else {
+                getLogger().warning("The /" + commandName + " command was not registered.");
+            }
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            getLogger().severe("Failed to unregister the /" + commandName + " command: " + e.getMessage());
+        }
+    }
 
     @Override
     public void onDisable() {
